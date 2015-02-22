b183c72c7065e52ad5da72299692b0d0fe8f14d3
Jason B <bjason@gmail.com>
2015-02-21 21:02:46 -0800

    playing with spring

---
+++ fff/fff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fff
+//
+//  Created by Jason Bautista on 2/21/15.
+//  Copyright (c) 2015 Jason Bautista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
